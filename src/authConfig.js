b158c600a2077a3258ad9a76a1896324cd534c91
Lindokuhle777 <92369482+Lindokuhle777@users.noreply.github.com>
2022-05-02 21:25:56 +0200

    first commit

--- src/authConfig.js
+++ src/authConfig.js
@@ -12,10 +12,10 @@ import { LogLevel } from "@azure/msal-browser";
  */
 export const msalConfig = {
     auth: {
-        clientId: "Enter_the_Application_Id_Here",
-        authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
-        redirectUri: "Enter_the_Redirect_Uri_Here"
-    },
+        clientId: "5eca034e-a0bb-4fe8-a92b-3aacea48d39e",
+        authority: "https://login.microsoftonline.com/4b1b908c-5582-4377-ba07-a36d65e34934", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+        redirectUri: "http://localhost:3000",
+      },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
         storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
@@ -60,5 +60,5 @@ export const loginRequest = {
  * https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-browser/docs/resources-and-scopes.md
  */
 export const graphConfig = {
-    graphMeEndpoint: "Enter_the_Graph_Endpoint_Herev1.0/me"
+    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
 };
